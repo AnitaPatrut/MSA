d27099fdcca5833a03d409fac62e9e49095c6edd
Anita <anitaalexandra98@yahoo.com>
2020-10-22 13:26:08 +0300

    Created view for the levels menu and configured play, option, quit and level 1 buttons

--- Game/Assets/Scenes/Menu.cs
+++ Game/Assets/Scenes/Menu.cs
@@ -8,7 +8,13 @@ public class Menu : MonoBehaviour
   
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+          SceneManager.LoadScene("Level 1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
