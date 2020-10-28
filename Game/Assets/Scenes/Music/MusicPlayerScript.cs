ec3cd6f6f47e81cb033295aef99154f6ec6f1b6f
Anita <anitaalexandra98@yahoo.com>
2020-10-28 19:40:42 +0200

    Added volume slider and background music, created login button

---
+++ Game/Assets/Scenes/Music/MusicPlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour
+{
+    private AudioSource audioSrc;
+    private float musicVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
