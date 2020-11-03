20716a1b782eb69cda3042d9da9b6a957ca81c26
Anita <anitaalexandra98@yahoo.com>
2020-11-04 00:27:05 +0200

    Created idle, walking and running movements for the player.

---
+++ Game/Assets/Models/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class Movement : MonoBehaviour
+{
+    Animator anim;
+   
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        anim.SetFloat("isWalking", Input.GetAxis("Vertical"));
+    }
+}
