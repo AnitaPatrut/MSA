cf0021db8a558b5441b0a356ae6164bbd56f5879
Anita <anitaalexandra98@yahoo.com>
2020-11-19 01:18:19 +0200

    Created a database + made a C# script to iterate over all the entries from the DataBase in order to check if the player is registered. (The primary key of the table from the DB is the username)

---
+++ Game/Assets/Scenes/DatabaseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW itemsData = new WWW("http://localhost/Users/ItemsData.php");
+        yield return itemsData;
+        string items = itemsData.text;
+        print(items);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
